d01726f56ebed9a10bf9e515de9064a3c0e4f695
Amar Brisco <amar.r.brisco@accenture.com>
2015-05-05 13:58:05 -0500

    Added Bootstrap and a few other packages

--- module1.js
+++ module1.js
@@ -2,5 +2,6 @@ var _ = require('underscore');
 var hint = require('jshint');
 var gulp = require('gulp');
 var m2 = require('./module2');
+var bootstrap = require('bootstrap');
 //m2();
-console.log(gulp);
+console.log(bootstrap);

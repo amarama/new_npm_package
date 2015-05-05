056c0d2ba0d89a24cbc2314c320549e747caed8d
Amar Brisco <amar.r.brisco@accenture.com>
2015-05-05 09:59:18 -0500

    commit

---
+++ module1.js
@@ -0,0 +1,6 @@
+var _ = require('underscore');
+var hint = require('jshint');
+var gulp = require('gulp');
+var m2 = require('./module2');
+//m2();
+console.log(gulp);
---
+++ module2.js
@@ -0,0 +1,10 @@
+var a = 2;
+var b = 4;
+/*
+exports.a = a;
+exports.b = b;
+exports.c = a * b;
+*/
+module.exports = function () {
+    console.log('module2!');
+};
